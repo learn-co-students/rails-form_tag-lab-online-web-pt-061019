ecd29ee2cda3cc59fd12b9855f382c88cb908e49
Oscar Shoudt <shoudto@gmail.com>
2020-03-09 21:52:05 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,18 @@
 class StudentsController < ApplicationController
   
   def index
-    @students = Student.all
+    @students = Student.all # presenting all students
   end
 
   def show
-    @student = Student.find(params[:id])
+    @student = Student.find(params[:id]) # show a particular student
   end
 
-  def new 
+  def new  #  creating a new student with a form
   end 
 
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name]) # creating the student
     redirect_to students_path
   end 
 end
